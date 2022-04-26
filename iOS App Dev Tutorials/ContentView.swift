b5d3ae36dd5d939d35a53c5b0ef0481ae128e753
Amjadoudeh <Amjad.oudeh88@gmail.com>
2022-04-26 13:10:49 +0200

    seeing the values of UInt8

--- iOS App Dev Tutorials/ContentView.swift
+++ iOS App Dev Tutorials/ContentView.swift
@@ -6,14 +6,21 @@ struct ContentView: View {
 
     /// changing the values of both let and var:
     /// so it will change the value of the var and show error when we try to change the value of let because it constant and won't be changed
+    let minValue = UInt8.min
+    
+    
+    // minValue is equal to 0, and is of type UInt8
+    let maxValue = UInt8.max
     
     var body: some View {
         VStack {
             Spacer()
             Text("\(maximumNumberOfLoginAttempts)").font(.largeTitle).bold()
+            Text("\(minValue)").font(.largeTitle).bold()
         Spacer()
         Text("\(currentLoginAttempt)")
                 .font(.largeTitle).bold()
+            Text("\(maxValue)").font(.largeTitle).bold()
             Spacer()
         }
     }
