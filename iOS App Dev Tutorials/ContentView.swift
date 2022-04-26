0f2d803c5c2909ae067d640069029af65dca604d
Amjadoudeh <Amjad.oudeh88@gmail.com>
2022-04-26 12:56:28 +0200

    var and let / added a commmnet

--- iOS App Dev Tutorials/ContentView.swift
+++ iOS App Dev Tutorials/ContentView.swift
@@ -3,6 +3,9 @@ import SwiftUI
 struct ContentView: View {
     let maximumNumberOfLoginAttempts = 10
     var currentLoginAttempt = 0
+
+    /// changing the values of both let and var:
+    /// so it will change the value of the var and show error when we try to change the value of let because it constant and won't be changed
     
     var body: some View {
         VStack {
