e21f92f0926377cf5a74cd2bea0ae1019f81a20c
Amjadoudeh <Amjad.oudeh88@gmail.com>
2022-04-26 12:49:09 +0200

    var and let types

--- iOS App Dev Tutorials/ContentView.swift
+++ iOS App Dev Tutorials/ContentView.swift
@@ -1,9 +1,18 @@
 import SwiftUI
 
 struct ContentView: View {
+    let maximumNumberOfLoginAttempts = 10
+    var currentLoginAttempt = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Spacer()
+            Text("\(maximumNumberOfLoginAttempts)").font(.largeTitle).bold()
+        Spacer()
+        Text("\(currentLoginAttempt)")
+                .font(.largeTitle).bold()
+            Spacer()
+        }
     }
 }
 
